c8116104244d93401e796f30a8965ec1279dba47
Nathan0674 <101610894+Nathan0674@users.noreply.github.com>
2022-08-17 10:22:16 +1200

    Added alt player states

--- 12DTM Major Game/Assets/Scripts/PlayerController.cs
+++ 12DTM Major Game/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D body;
-
-    float horizontal;
-    float vertical;
+    public SpriteRenderer spriteRenderer;
+    public Sprite playerDark;
+    public Sprite playerLight;
+    public float horizontal;
+    public float vertical;
+    public int swapNumber;
 
     public float runSpeed = 10.0f;
 
     private void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         body = GetComponent<Rigidbody2D>();  
     }
 
@@ -26,5 +30,29 @@ public class PlayerController : MonoBehaviour
             body.velocity = new Vector2(body.velocity.x, vertical * runSpeed);
         }
         body.velocity = new Vector2(horizontal * runSpeed, body.velocity.y);
+        
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            spriteRenderer.flipX = true;
+        } 
+
+        if(Input.GetKeyDown("x"))
+        {
+            swapNumber += 1;
+            
+            if(swapNumber%2==0)
+            {
+                spriteRenderer.sprite = playerDark; 
+            }
+            else
+            {
+                spriteRenderer.sprite = playerLight; 
+            }
+        }
     }
 }
