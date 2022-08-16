f009ff258e5fee6329d26769b6e1bb6506f1e468
Nathan0674 <101610894+Nathan0674@users.noreply.github.com>
2022-08-17 11:11:49 +1200

    Added Box Cast to check for jumping

--- 12DTM Major Game/Assets/Scripts/PlayerController.cs
+++ 12DTM Major Game/Assets/Scripts/PlayerController.cs
@@ -5,27 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D body;
-    public SpriteRenderer spriteRenderer;
+    private BoxCollider2D bc;
+    private SpriteRenderer spriteRenderer;
+    public LayerMask groundLayer;
     public Sprite playerDark;
     public Sprite playerLight;
+    public GameObject groundCheck;
+
     public float horizontal;
     public float vertical;
     public int swapNumber;
-
+    public int playerState;
     public float runSpeed = 10.0f;
+    private bool isGrounded;
 
     private void Start()
     {
+        bc = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         body = GetComponent<Rigidbody2D>();  
     }
 
     void Update()
     {
+        isGrounded = Physics2D.OverlapBox(groundCheck.transform.position, new Vector2(bc.bounds.size.x - 0.1f, 0.1f), 0f, groundLayer);
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 
-        if(vertical != 0)
+        if(vertical != 0 && isGrounded)
         {
             body.velocity = new Vector2(body.velocity.x, vertical * runSpeed);
         }
